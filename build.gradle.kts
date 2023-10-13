7414c6b90c10b9e03b728fd5374d1095c3894666
Tony Rozzi <arozzi@calpoly.edu>
2023-10-12 22:28:37 -0700

    Downloaded and added OpenGL dependencies to gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,13 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation("java3d:j3d-core:1.3.1");
+
+    implementation(fileTree(mapOf("dir" to "jar/", "include" to listOf("*.jar"))))
+    runtimeOnly(fileTree(mapOf("dir" to "jar/", "include" to listOf("*.jar"))))
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
